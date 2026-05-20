12d4ccb36361ea3eb8ccf2bcaf9ce4cc449baa61
RyanLouw <110169633+RyanLouw@users.noreply.github.com>
2026-05-20 15:23:56 +0200

    Add gitignore, remove generated files, and fix AppHost compile errors

--- AppHost1/AppHost.cs
+++ AppHost1/AppHost.cs
@@ -1,22 +1,3 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-
-var postgres = builder.AddPostgres("rentalapp-postgres")
-    .WithLifetime(ContainerLifetime.Persistent)
-    .WithDataVolume("rentalapp-postgres");
-
-var ExameDb = postgres.AddDatabase("ExameDb");
-
-
-var migrations = builder.AddProject<>("database-migrations")
-    .WithEnvironment("ASPNETCORE_ENVIRONMENT", "Development")
-    .WithReference(ExameDb)
-    .WaitFor(ExameDb);
-
-
-builder.AddProject<Projects.ExamRosterApp>("web")
-    .WithExplicitStart()
-    .WithReference(ExameDb);
-    
-
 await builder.Build().RunAsync();
